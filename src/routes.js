871f4d837f7202361bee42e9ac60a4fa7a2752ae
Gilson da Gama <gilsondagama@hotmail.com>
2020-09-12 17:29:17 -0300

    Criando rotas

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+
+// import { Container } from './styles';
+
+function src() {
+  return <div />;
+}
+
+export default src;
